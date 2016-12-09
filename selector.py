749d58d47fcd8aaf627f2423a9f7ef2d921bd4ac
Ibrahim Aljarah <aljarrahcs@gmail.com>
2016-12-09 02:51:50 +0200

    update selector

--- selector.py
+++ selector.py
@@ -10,7 +10,6 @@ import GWO as gwo
 import MFO as mfo
 import CS as cs
 import BAT as bat
-import benchmarks
 import csv
 import numpy
 import time
@@ -19,33 +18,39 @@ import costNN
 import evaluateNetClassifier as evalNet
 import solution
 
-def selector(algo,func_details,popSize,Iter):
+def selector(algo,func_details,popSize,Iter,trainDataset,testDataset):
     function_name=func_details[0]
     lb=func_details[1]
     ub=func_details[2]
     
     DatasetSplitRatio=2/3
     
+    dataTrain="datasets/"+trainDataset
+    dataTest="datasets/"+testDataset
+    
+    Dataset_train=numpy.loadtxt(open(dataTrain,"rb"),delimiter=",",skiprows=0)
+    Dataset_test=numpy.loadtxt(open(dataTest,"rb"),delimiter=",",skiprows=0)
+      
+    
+    numRowsTrain=numpy.shape(Dataset_train)[0]    # number of instances in the train dataset
+    numInputsTrain=numpy.shape(Dataset_train)[1]-1 #number of features in the train dataset
+
+    numRowsTest=numpy.shape(Dataset_test)[0]    # number of instances in the test dataset
+    
+    numInputsTest=numpy.shape(Dataset_test)[1]-1 #number of features in the test dataset
  
-    Dataset=numpy.loadtxt(open("train.csv","rb"),delimiter=",",skiprows=0)
-    
-    numRows=numpy.shape(Dataset)[0]    # number of instances in the dataset
-    numInputs=numpy.shape(Dataset)[1]-1 #number of features in the dataset
 
-    splitPoint= int(DatasetSplitRatio*numRows)
-
-    trainInput=Dataset[0:splitPoint,0:-1]
-    trainOutput=Dataset[0:splitPoint,-1]
+    trainInput=Dataset_train[0:numRowsTrain,0:-1]
+    trainOutput=Dataset_train[0:numRowsTrain,-1]
     
-    testInput=Dataset[splitPoint:,0:-1]
-    testOutput=Dataset[splitPoint:,-1]
-   
+    testInput=Dataset_test[0:numRowsTest,0:-1]
+    testOutput=Dataset_test[0:numRowsTest,-1]
     
     #number of hidden neurons
-    HiddenNeurons = numInputs*2+1
-    net = nl.net.newff([[0, 1]]*numInputs, [HiddenNeurons, 1])
+    HiddenNeurons = numInputsTrain*2+1
+    net = nl.net.newff([[0, 1]]*numInputsTrain, [HiddenNeurons, 1])
     
-    dim=(numInputs*HiddenNeurons)+(2*HiddenNeurons)+1;
+    dim=(numInputsTrain*HiddenNeurons)+(2*HiddenNeurons)+1;
     
     if(algo==0):
         x=pso.PSO( getattr(costNN, function_name),lb,ub,dim,popSize,Iter,trainInput,trainOutput,net)
