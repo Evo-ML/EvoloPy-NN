a48c74ad2af3308b68ac3763ea05813471cbcec6
Ibrahim Aljarah <aljarrahcs@gmail.com>
2016-12-09 02:51:04 +0200

    update main

--- main.py
+++ main.py
@@ -4,24 +4,18 @@ import numpy
 import time
 import selector as slctr
 
-
-
-
 # Select optimizers
 PSO= True
 MVO= True
-GWO = True
+GWO = False
 MFO= False
 CS= False
 BAT=False
 
 
-# Select benchmark function
-BreastCancer=True
-
 
 optimizer=[PSO, MVO, GWO, MFO, CS, BAT]
-benchmarkfunc=[BreastCancer] 
+datasets=["BreastCancer", "Diabetes", "Liver", "Parkinsons", "Vertebral"]
         
 # Select number of repetitions for each experiment. 
 # To obtain meaningful statistical results, usually 30 independent runs 
@@ -30,7 +24,7 @@ NumOfRuns=2
 
 # Select general parameters for all optimizers (population size, number of iterations)
 PopulationSize = 50
-Iterations= 15
+Iterations= 5
 
 #Export results ?
 Export=True
@@ -49,22 +43,26 @@ CnvgHeader=[]
 for l in range(0,Iterations):
 	CnvgHeader.append("Iter"+str(l+1))
 
-
-for i in range (0, len(optimizer)):
-    for j in range (0, len(benchmarkfunc)):
-        if((optimizer[i]==True) and (benchmarkfunc[j]==True)): # start experiment if an optimizer and an objective function is selected
+trainDataset="breastTrain.csv"
+testDataset="breastTest.csv"
+for j in range (0, 2):        # specfiy the number of the datasets
+    for i in range (0, len(optimizer)):
+    
+        if((optimizer[i]==True)): # start experiment if an optimizer and an objective function is selected
             for k in range (0,NumOfRuns):
                 
                 func_details=["costNN",-1,1]
-                x=slctr.selector(i,func_details,PopulationSize,Iterations)
+                trainDataset=datasets[j]+"Train.csv"
+                testDataset=datasets[j]+"Test.csv"
+                x=slctr.selector(i,func_details,PopulationSize,Iterations,trainDataset,testDataset)
                   
                 if(Export==True):
                     with open(ExportToFile, 'a',newline='\n') as out:
                         writer = csv.writer(out,delimiter=',')
                         if (Flag==False): # just one time to write the header of the CSV file
-                            header= numpy.concatenate([["Optimizer","objfname","startTime","EndTime","ExecutionTime","trainAcc", "trainTP","trainFN","trainFP","trainTN", "testAcc", "testTP","testFN","testFP","testTN"],CnvgHeader])
+                            header= numpy.concatenate([["Optimizer","Dataset","objfname","Experiment","startTime","EndTime","ExecutionTime","trainAcc", "trainTP","trainFN","trainFP","trainTN", "testAcc", "testTP","testFN","testFP","testTN"],CnvgHeader])
                             writer.writerow(header)
-                        a=numpy.concatenate([[x.optimizer,x.objfname,x.startTime,x.endTime,x.executionTime,x.trainAcc, x.trainTP,x.trainFN,x.trainFP,x.trainTN, x.testAcc, x.testTP,x.testFN,x.testFP,x.testTN],x.convergence])
+                        a=numpy.concatenate([[x.optimizer,datasets[j],x.objfname,k+1,x.startTime,x.endTime,x.executionTime,x.trainAcc, x.trainTP,x.trainFN,x.trainFP,x.trainTN, x.testAcc, x.testTP,x.testFN,x.testFP,x.testTN],x.convergence])
                         writer.writerow(a)
                     out.close()
                 Flag=True # at least one experiment
